1f29ebd35d4434f6c68bdb1bbca5032f2f235261
Alex Hart <alex@hartcode.com>
2016-01-15 11:21:19 -0600

    Additional Fab icon change
    Need to also trigger fab icon change when the add location menu item
    is selected. Also the fab icon now changes back when the back/up buttons
    are pressed.

--- app/src/main/java/com/hartcode/hartweather/list/WeatherListActivity.java
+++ app/src/main/java/com/hartcode/hartweather/list/WeatherListActivity.java
@@ -6,6 +6,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.*;
 import android.support.design.widget.*;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.*;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -19,7 +20,7 @@ import com.hartcode.libweatherapi.libopenweatherapi.*;
 
 import org.slf4j.*;
 
-public class WeatherListActivity extends AppCompatActivity implements View.OnClickListener, IConnectivity {
+public class WeatherListActivity extends AppCompatActivity implements View.OnClickListener, MenuItemCompat.OnActionExpandListener, IConnectivity {
     private static final Logger logger = LoggerFactory.getLogger(WeatherListActivity.class);
 
     private String api_key = "34b3e14b5a4abd6edcc4c2e4051a6cab";
@@ -40,21 +41,19 @@ public class WeatherListActivity extends AppCompatActivity implements View.OnCli
         this.model = new Model();
         SharedPreferences prefs = getSharedPreferences(getString(R.string.preference_key), Context.MODE_PRIVATE);
         String temp_unit_string = prefs.getString(getString(R.string.temp_unit_key), Unit.Fahrenheit.toString());
-        if (temp_unit_string != null)
-        {
+        if (temp_unit_string != null) {
             this.units = Unit.valueOf(temp_unit_string);
         }
 
         this.connectivityManager =
-                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
 
         IWeatherAPI weatherapi = new OpenWeatherMapWeatherAPI(this.api_key, this.units);
         this.networkManager = new NetworkManager(weatherapi, model, this);
 
 
-        for (int i =0; i < this.model.weatherSize(); i++)
-        {
+        for (int i = 0; i < this.model.weatherSize(); i++) {
             this.networkManager.addRequest(this.model.getItem(i));
         }
 
@@ -84,10 +83,11 @@ public class WeatherListActivity extends AppCompatActivity implements View.OnCli
         this.searchView = (SearchView) searchMenuItem.getActionView();
         this.searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         this.searchView.setIconifiedByDefault(true);
+        MenuItemCompat.setOnActionExpandListener(this.searchMenuItem,this);
         if (this.isSearchShown) {
             this.searchMenuItem.expandActionView();
         }
-        if (this.searchText != null && this.searchText != "") {
+        if (this.searchText != null && this.searchText.length() > 0) {
             this.searchView.setQuery(searchText, false);
         }
         return true;
@@ -95,13 +95,10 @@ public class WeatherListActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            // todo(alex) goto setting activity
             return true;
         }
 
@@ -122,53 +119,58 @@ public class WeatherListActivity extends AppCompatActivity implements View.OnCli
      */
     @Override
     public void onClick(View v) {
-            if (!this.searchMenuItem.isActionViewExpanded()) {
-                // Enable search
-                this.searchMenuItem.expandActionView();
-                // change fab to search button
-                this.floatingActionButton.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_search));
+        if (!this.searchMenuItem.isActionViewExpanded()) {
+            // Enable search
+            this.searchMenuItem.expandActionView();
+        } else {
+            // If fab is in search mode than submit search
+            CharSequence queryText = this.searchView.getQuery();
+            if (queryText != null && queryText.length() > 0) {
+                this.searchView.setQuery(queryText, true);
             }
-            else
-            {
-                // If fab is in search mode than submit search
-                CharSequence queryText = this.searchView.getQuery();
-                if (queryText != null && queryText.length() > 0) {
-                    this.searchView.setQuery(queryText, true);
-                    this.floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.fab_add));
-                }
-            }
+        }
     }
 
     @Override
     public boolean isConnectionActive() {
         boolean retval;
         NetworkInfo networkInfo = this.connectivityManager.getActiveNetworkInfo();
-        retval = (networkInfo != null && networkInfo.isConnected()) ;
+        retval = (networkInfo != null && networkInfo.isConnected());
         return retval;
     }
 
     @Override
-    public void onSaveInstanceState(Bundle bundle)
-    {
-        if (this.searchMenuItem != null)
-        {
+    public void onSaveInstanceState(Bundle bundle) {
+        if (this.searchMenuItem != null) {
             this.isSearchShown = this.searchMenuItem.isActionViewExpanded();
-            this.floatingActionButton.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_search));
-        }
-        else
-        {
+        } else {
             this.isSearchShown = false;
         }
 
-        if (this.searchView != null)
-        {
+        if (this.searchView != null) {
             this.searchText = this.searchView.getQuery();
-        }else
-        {
+        } else {
             this.searchText = "";
         }
-        bundle.putBoolean("isSearchShown",this.isSearchShown);
+        bundle.putBoolean("isSearchShown", this.isSearchShown);
         bundle.putCharSequence("searchText", this.searchText);
         super.onSaveInstanceState(bundle);
+    }
+
+    @Override
+    public boolean onMenuItemActionExpand(MenuItem item) {
+        if (item.getItemId() == R.id.menu_add) {
+            // change fab to search button
+            this.floatingActionButton.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_search));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemActionCollapse(MenuItem item) {
+        if (item.getItemId() == R.id.menu_add) {
+            this.floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.fab_add));
+        }
+        return true;
     }
 }
