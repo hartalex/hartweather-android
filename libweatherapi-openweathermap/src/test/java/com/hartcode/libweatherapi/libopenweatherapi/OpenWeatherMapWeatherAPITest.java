be8d97570955ac11050b06fe1951704faa96d1a2
Alex Hart <alex@hartcode.com>
2016-01-25 08:49:31 -0600

    Add GPLv3 blurb to each java source file

--- libweatherapi-openweathermap/src/test/java/com/hartcode/libweatherapi/libopenweatherapi/OpenWeatherMapWeatherAPITest.java
+++ libweatherapi-openweathermap/src/test/java/com/hartcode/libweatherapi/libopenweatherapi/OpenWeatherMapWeatherAPITest.java
@@ -1,3 +1,24 @@
+/**
+*
+*    HartWeather - A Simple Weather Android App
+*    Copyright (C) 2016  Alex Hart
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see 
+*    <https://github.com/hartcode/hartweather-android/blob/master/LICENSE>.
+*
+*/
+
 package com.hartcode.libweatherapi.libopenweatherapi;
 
 import com.hartcode.hartweather.libweatherapi.*;
@@ -16,6 +37,7 @@ public class OpenWeatherMapWeatherAPITest extends TestCase {
 
     // Normally the api key would be pulled from a private server and not hardcoded.
     // However since the api doesn't support ssl it is trivial to scrape the key from http logs.
+	// TODO:  Need to pull from this environment variable instead.
     private final static String API_KEY = "2de143494c0b295cca9337e1e96b00e0";
     private IWeatherAPI api;
 
